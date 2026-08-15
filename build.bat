9883feeb4a4146645d4cf9aeb6ebc2ffdd5c7111
Wizardtastic <Walkertastic@gmail.com>
2026-08-15 19:40:12 -0400

    Quick Cleaning

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Double-click this to build the minified dist\ copy of Qalcom OS.
-REM It just runs build-qalcom.ps1 with the execution policy bypassed for this one call.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0build-qalcom.ps1" %*
-echo.
-pause
